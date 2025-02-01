8fd50315863205f5463a1cd5c2e872f8b0b70c94
Tomas <tbecerra810@gmail.com>
2025-02-01 19:51:35 -0300

    Ahora si

--- js/script.js
+++ js/script.js
@@ -1,10 +1,19 @@
 document.addEventListener("DOMContentLoaded", function () {
     const menuItems = document.querySelectorAll("ul li");
-
     menuItems.forEach((item) => {
         item.addEventListener("click", function () {
             menuItems.forEach((el) => el.classList.remove("active"));
             this.classList.add("active");
         });
     });
+
+    const toggleBtn = document.querySelector('.toggle-btn')
+    const toggleBtnIcon = document.querySelector('.toggle-btn i')
+    const dropBoxMenu = document.querySelector('.dropbox-menu')
+
+    toggleBtn.onclick = function() {
+        dropBoxMenu.classList.toggle('open')
+        const isOpen = dropBoxMenu.classList.contains('open')
+        toggleBtnIcon.classList = isOpen ? 'fa-solid fa-xmark' : 'fa-solid fa-bars'
+    }
 });
